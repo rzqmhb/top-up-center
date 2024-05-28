d7b107ccfb83f491057f74056e631eac3589c949
rzqmhb <roziqmahbubi22@gmail.com>
2024-05-28 10:10:34 +0700

    refactor: simplify dsn in .env

--- database/postgres.go
+++ database/postgres.go
@@ -1,28 +1,18 @@
 package database
 
 import (
-	"fmt"
 	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-
-var (
-	// get db creds from loaded env
-	host = os.Getenv("DB_HOST")
-	user = os.Getenv("DB_USER")
-	password = os.Getenv("DB_PASSWORD")
-	dbname = os.Getenv("DB_NAME")
-	port = os.Getenv("DB_PORT")
-)
+// get DSN for postgres database from loaded .env file
+var dsn = os.Getenv("DSN")
 
 // connecting to postgres database using gorm with the provided credentials
 func ConnectDB() (*gorm.DB, error) {
-	dbinfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-									host, user, password, dbname, port)
-	db, err := gorm.Open(postgres.Open(dbinfo), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
