210db1746793904cf7d2a5cfe1ea3c8e64d34d12
rzqmhb <roziqmahbubi22@gmail.com>
2024-05-28 12:53:39 +0700

    feat: add models & few db operations

--- database/postgres.go
+++ database/postgres.go
@@ -1,20 +1,215 @@
 package database
 
 import (
+	"errors"
 	"os"
+
+	"github.com/rzqmhb/top-up-center/models"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+type PostgresDB struct {
+	DB *gorm.DB
+}
+
 // get DSN for postgres database from loaded .env file
 var dsn = os.Getenv("DSN")
 
 // connecting to postgres database using gorm with the provided credentials
-func ConnectDB() (*gorm.DB, error) {
+func InitDB() (*PostgresDB, error) {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-	return db, nil
+	return &PostgresDB{DB: db}, nil
+}
+
+//------------------------------------------
+// DML and DQL operations to 'users' table
+//------------------------------------------
+
+func (postgres *PostgresDB) StoreUser(user models.User) error {
+	result := postgres.DB.Create(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (postgres *PostgresDB) FetchUsers() ([]models.User, error) {
+	var users []models.User = []models.User{}
+	result := postgres.DB.Find(&users)
+	if result.Error != nil {
+		return users, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return users, errors.New("no data found")
+	}
+	return users, nil
+}
+
+func (postgres *PostgresDB) FetchUserByUsername(username string) (models.User, error) {
+	var user models.User = models.User{}
+	result := postgres.DB.Raw("SELECT * FROM users WHERE name = ?", username).Scan(&user)
+	if result.Error != nil {
+		return user, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return user, errors.New("no data found")
+	}
+	return user, nil
+}
+
+func (postgres *PostgresDB) FetchUserByEmail(email string) (models.User, error) {
+	var user models.User = models.User{}
+	result := postgres.DB.Raw("SELECT * FROM users WHERE email = ?", email).Scan(&user)
+	if result.Error != nil {
+		return user, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return user, errors.New("no data found")
+	}
+	return user, nil
+}
+
+func (postgres *PostgresDB) UpdateUser(id int, user models.User) error {
+	result := postgres.DB.Model(&models.User{}).Where("id = ?", id).Updates(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("no data found")
+	}
+	return nil
+}
+
+func (postgres *PostgresDB) DeleteUser(id int) error {
+	result := postgres.DB.Delete(&models.User{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("no data found")
+	}
+	return nil
+}
+
+//---------------------------------------------
+// DML and DQL operations to 'sessions' table
+//---------------------------------------------
+
+func (postgres *PostgresDB) StoreSession(session models.Session) error {
+	result := postgres.DB.Create(&session)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (postgres *PostgresDB) FetchSessions() ([]models.Session, error) {
+	var sessions []models.Session = []models.Session{}
+	result := postgres.DB.Find(&sessions)
+	if result.Error != nil {
+		return sessions, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return sessions, errors.New("no data found")
+	}
+	return sessions, nil
+}
+
+func (postgres *PostgresDB) FetchSessionByToken(token string) (models.Session, error) {
+	var session models.Session = models.Session{}
+	result := postgres.DB.Raw("SELECT * FROM sessions WHERE token = ?", token).Scan(&session)
+	if result.Error != nil {
+		return session, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return session, errors.New("no data found")
+	}
+	return session, nil
+}
+
+func (postgres *PostgresDB) FetchSessionByUsername(username string) (models.Session, error) {
+	var session models.Session = models.Session{}
+	result := postgres.DB.Raw("SELECT * FROM sessions WHERE user_name = ?", username).Scan(&session)
+	if result.Error != nil {
+		return session, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return session, errors.New("no data found")
+	}
+	return session, nil
+}
+
+func(postgres *PostgresDB) UpdateSession(username string, session models.Session) error {
+	result := postgres.DB.Model(&models.User{}).Where("username = ?", username).Updates(&session)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("no data found")
+	}
+	return nil
+}
+
+func(postgres *PostgresDB) DeleteSession(token string) error {
+	result := postgres.DB.Delete(&models.Session{}, "token = ?", token)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("no data found")
+	}
+	return nil
+}
+
+//-------------------------------------------
+// DML and DQL operations to 'games' table
+//-------------------------------------------
+
+func (postgres *PostgresDB) StoreGame(game models.Game) error {
+	result := postgres.DB.Create(&game)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+//-------------------------------------------
+// DML and DQL operations to 'items' table
+//-------------------------------------------
+
+func (postgres *PostgresDB) StoreItem(item models.Item) error {
+	result := postgres.DB.Create(&item)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+//-------------------------------------------
+// DML and DQL operations to 'orders' table
+//-------------------------------------------
+
+func (postgres *PostgresDB) StoreOrder(order models.Order) error {
+	result := postgres.DB.Create(&order)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+//------------------------------------------------
+// DML and DQL operations to 'order_items' table
+//------------------------------------------------
+
+func (postgres *PostgresDB) StoreOrderItem(orderItem models.OrderItem) error {
+	result := postgres.DB.Create(&orderItem)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
--- models/models.go
+++ models/models.go
@@ -1,2 +1,49 @@
 package models
 
+import "time"
+
+type User struct {
+	ID        int       `json:"user_id"`
+	Name      string    `json:"user_name"`
+	Email     string    `json:"user_email"`
+	Password  string    `json:"user_password"`
+	CreatedAt time.Time `json:"user_created_at"`
+	UpdatedAT time.Time `json:"user_updated_at"`
+}
+
+type Session struct {
+	ID       int       `json:"session_id"`
+	UserName string    `json:"session_user_name"`
+	Token    string    `json:"session_token"`
+	Expiry   time.Time `json:"session_expiry"`
+}
+
+type Game struct {
+	ID        int       `json:"game_id"`
+	Name      string    `json:"game_name"`
+	CreatedAt time.Time `json:"game_created_at"`
+	UpdatedAT time.Time `json:"game_updated_at"`
+}
+
+type Item struct {
+	ID        int       `json:"item_id"`
+	GameID    int       `json:"item_game_id"`
+	Name      string    `json:"item_name"`
+	Price     float64   `json:"item_price"`
+	CreatedAt time.Time `json:"item_created_at"`
+	UpdatedAT time.Time `json:"item_updated_at"`
+}
+
+type Order struct {
+	ID     int       `json:"order_id"`
+	UserID int       `json:"order_user_id"`
+	Status string    `json:"order_status"`
+	Date   time.Time `json:"order_date"`
+}
+
+type OrderItem struct {
+	ID           int     `json:"order_item_id"`
+	OrderID      int     `json:"order_item_order_id"`
+	ItemID       int     `json:"order_item_item_id"`
+	CurrentPrice float64 `json:"order_item_current_price"`
+}
